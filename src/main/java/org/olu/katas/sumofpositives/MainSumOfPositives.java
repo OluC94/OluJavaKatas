2f99ab5e0baf5097cee79671e27413f66d4e7f9f
OluC94 <coker.olu94@gmail.com>
2024-07-31 09:34:32 +0100

    Initial commit

---
+++ src/main/java/org/olu/katas/sumofpositives/MainSumOfPositives.java
@@ -0,0 +1,7 @@
+package org.olu.katas.sumofpositives;
+
+public class MainSumOfPositives {
+    public static int sumOfPositives(int[] inputNumbers) {
+        return 12;
+    }
+}
---
+++ src/test/java/org/olu/katas/sumofpositives/MainSumOfPositivesTest.java
@@ -0,0 +1,10 @@
+package org.olu.katas.sumofpositives;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainSumOfPositivesTest {
+
+    @org.junit.jupiter.api.Test
+    void sumOfPositives() {
+    }
+}
