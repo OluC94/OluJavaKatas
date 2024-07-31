480f07d4b29ab1947293e42ba2f76d525a91c3bd
OluC94 <coker.olu94@gmail.com>
2024-07-31 11:10:07 +0100

    complete test cases for vowel count

--- src/test/java/org/olu/katas/vowelcount/MainVowelCountTest.java
+++ src/test/java/org/olu/katas/vowelcount/MainVowelCountTest.java
@@ -10,4 +10,25 @@ class MainVowelCountTest {
     void returnsZeroForEmptyString() {
         assertEquals(0, MainVowelCount.vowelCount(""));
     }
+
+    @Test
+    void correctResultForUniqueVowels() {
+        assertEquals(1, MainVowelCount.vowelCount("a"));
+        assertEquals(4, MainVowelCount.vowelCount("unique"));
+        assertEquals(2, MainVowelCount.vowelCount("vowel"));
+        assertEquals(3, MainVowelCount.vowelCount("with space"));
+    }
+
+    @Test
+    void correctResultForNonUniqueVowels() {
+        assertEquals(7, MainVowelCount.vowelCount("aaaaaaa"));
+        assertEquals(5, MainVowelCount.vowelCount("repitition"));
+        assertEquals(21, MainVowelCount.vowelCount("oooiieeuuaqwptrmnaiiiaaaoooe"));
+    }
+
+    @Test
+    void zeroForNoVowelsPresent(){
+        assertEquals(0, MainVowelCount.vowelCount("rhythm"));
+        assertEquals(0, MainVowelCount.vowelCount("xyz"));
+    }
 }
